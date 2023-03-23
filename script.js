4e37017f04d78be763a155601260ee51cf04b0a0
Kassia do Espirito Santo <kassiaes@gmail.com>
2023-03-23 18:04:55 -0300

    add css,img

--- script.js
+++ script.js
@@ -2,24 +2,33 @@
 const botao = document.querySelector('button')
 
 // leitura do clique do botão
-botao.addEventListener('click', () => {
+botao.addEventListener('click',async () => {
     // caso seja clicado inicia com o processo randomico
     const numAleatorio = parseInt(Math.random() * 2)
-    deuErro(numAleatorio)
-    .then(() => {
-        console.log(`${numAleatorio} é impar`)
+    // funçao deuErro para testar se erro ou sucesso
+    await deuErro(numAleatorio)
+    // quando sucesso
+    .then((valor) => {
+        console.log(valor)
     })
-    .catch(() => {
-        console.log(`${numAleatorio} é par`)
-    })    
+    // quando erro
+    .catch((error) => {
+        console.log(error)
+    })  
+    // sempre cai em uma condição
+    .finally((data) => {
+        console.log(data)
+    })
+    
 })
 
+// função considerou se par é sucesso e se ímpar é erro
 const deuErro = (numAleatorio) => {
     return new Promise ((resolve, reject) => {
         if(numAleatorio % 2 === 0) {
-            reject(numAleatorio)
+            reject()
         } else {            
-            resolve(`${numAleatorio} é impar`)
+            resolve()
         }
     })
 }
