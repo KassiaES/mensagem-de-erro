ba8bf85ebbe24ffae2dc2d8a9a31aa12af4d3019
Kassia do Espirito Santo <kassiaes@gmail.com>
2023-03-23 19:38:27 -0300

    promise

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 // construção do botão 
 const botao = document.querySelector('button')
+const mensagem = document.createElement('p')
+const root = document.getElementById('root')
+root.appendChild(mensagem)
+const imagem = document.createElement('img')
+root.appendChild(imagem)
 
 // leitura do clique do botão
-botao.addEventListener('click',async () => {
+botao.addEventListener('click', () => {
+    
+
     // caso seja clicado inicia com o processo randomico
     const numAleatorio = parseInt(Math.random() * 2)
     // funçao deuErro para testar se erro ou sucesso
-    await deuErro(numAleatorio)
+    deuErro(numAleatorio)
     // quando sucesso
     .then((valor) => {
         console.log(valor)
-    })
+        mensagem.textContent = "SUCESSO"
+        imagem.src = "carregando.gif"
+
+    }) 
     // quando erro
     .catch((error) => {
         console.log(error)
+
+
     })  
+    
     // sempre cai em uma condição
     .finally((data) => {
         console.log(data)
@@ -26,9 +39,9 @@ botao.addEventListener('click',async () => {
 const deuErro = (numAleatorio) => {
     return new Promise ((resolve, reject) => {
         if(numAleatorio % 2 === 0) {
-            reject()
+            reject(false)
         } else {            
-            resolve()
+            resolve(true)
         }
     })
 }
