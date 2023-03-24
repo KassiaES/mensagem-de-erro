fd3579899434e40b82e112732a81d97febafee52
Kassia do Espirito Santo <kassiaes@gmail.com>
2023-03-24 11:50:04 -0300

    final work

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // construção do botão 
 const botao = document.querySelector('button')
-const mensagem = document.createElement('p')
+// definição da div onde serão criados os elementos
 const root = document.getElementById('root')
-root.appendChild(mensagem)
+// construção do elemento parágrafo 
+const linha = document.createElement('p')
+// implementação do parágrafo na div
+root.appendChild(linha)
+// construção do elemento imagem
 const imagem = document.createElement('img')
+// implementação da imagem na div
 root.appendChild(imagem)
 
+// Processo 1
 // leitura do clique do botão
-botao.addEventListener('click', () => {
-    
-
+botao.addEventListener('click', () => {  
     // caso seja clicado inicia com o processo randomico
     const numAleatorio = parseInt(Math.random() * 2)
     // funçao deuErro para testar se erro ou sucesso
@@ -17,22 +21,25 @@ botao.addEventListener('click', () => {
     // quando sucesso
     .then((valor) => {
         console.log(valor)
-        mensagem.textContent = "SUCESSO"
-        imagem.src = "carregando.gif"
+        linha.textContent = "SUCESSO"
+        imagem.src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGJmNjBkMzkxY2M4YmVkZTEwY2E3NDRlZjNlZmMxOGYxOGRkYWYxNyZjdD1z/h3uet8jAm1sJZhcqsL/giphy.gif"
 
     }) 
     // quando erro
     .catch((error) => {
         console.log(error)
+        linha.textContent = "ERRO"
+        imagem.src = "https://www.estudopratico.com.br/wp-content/uploads/2018/07/bug-computador.jpg"
 
-
-    })  
-    
-    // sempre cai em uma condição
+    })      
+    // sempre cai nesta condição no final
     .finally((data) => {
-        console.log(data)
-    })
-    
+        console.log("é para apagar")
+        setTimeout(() => {               
+            linha.textContent =""
+            imagem.src =""
+        }, 3000);
+    })    
 })
 
 // função considerou se par é sucesso e se ímpar é erro
@@ -45,3 +52,6 @@ const deuErro = (numAleatorio) => {
         }
     })
 }
+
+
+
